bd38dd46c48adc9360ef83bad2385dd83a529b4f
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2023-06-08 16:54:39 -0700

    basic notes

--- big O/bigo.js
+++ big O/bigo.js
@@ -1,1 +1,8 @@
-console.log('big o');
+// console.log('big o');
+// 1	Constant	statement, one line of code;
+// log(n)	Logarithmic	Divide and conquer(binary search)
+// n	Linear	Loop;
+// n * log(n)	Linearithmic	Effective sorting algorithms;
+// n ^ 2	Quadratic	Double loop;
+// n ^ 3	Cubic	Triple loop;
+// 2 ^ n	Exponential	Complex full search;
